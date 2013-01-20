89a7d7dac1a2f7dbdd9dec13310271f4a908e61b
Corey Scott <corey.scott@gmail.com>
2013-01-20 18:00:23 +0800

    added same code for blog entry 20130120

---
+++ org.sage42.simple_buttons/gen/com/example/org/sage42/simple_buttons/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.org.sage42.simple_buttons;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
